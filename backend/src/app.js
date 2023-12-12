0ff1456901e7234bbcf140d96ec39801e5805866
(Alex)andre Cardoso <100371225+BIGTOST@users.noreply.github.com>
2023-12-12 17:12:00 +0000

    b

--- backend/src/app.js
+++ backend/src/app.js
@@ -87,7 +87,7 @@ app.post('/upload', function(req, res) {
 });
 
 app.use('/image', (req,res)=>{
-    res.sendFile(publicFolder+'/    download.jpeg');
+    res.sendFile(publicFolder+'/download.jpeg');
 })
 
 //! lembrete, rota raiz deve ser sempre a mais em baixo
